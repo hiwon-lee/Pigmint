e7c1df86846d26d5fc2e721451b3939fe3184745
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-08-06 20:59:24 +0900

    Revert "chore: fix ko to en"
    This reverts commit 5332280fae83c3e939811035f058d4a55db804ea.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO ledger (description, amount, memo, transaction_date) VALUES ('lunch', 8000, 'full', '2025-08-06');
-INSERT INTO ledger (description, amount, transaction_date) VALUES ('subway fee', 1400, '2025-08-06');
-INSERT INTO ledger (description, amount, memo, transaction_date) VALUES ('coffee', 4500, 'starbucks', '2025-08-05');
+INSERT INTO ledger (description, amount, memo, transaction_date) VALUES ('점심값 (국밥)', 8000, '든든했다', '2025-08-06');
+INSERT INTO ledger (description, amount, transaction_date) VALUES ('지하철 요금', 1400, '2025-08-06');
+INSERT INTO ledger (description, amount, memo, transaction_date) VALUES ('커피', 4500, '메가커피', '2025-08-05');
